cabb9cb263c6c43fa141c15648b83b84c26ec1e9
Steven Haryanto <stevenharyanto@gmail.com>
2011-08-11 05:37:04 +0000

    Import of SHARYANTO/Task-Data-Format-Pretty-0.01 from CPAN.
    gitpan-cpan-distribution: Task-Data-Format-Pretty
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         SHARYANTO/Task-Data-Format-Pretty-0.01.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

---
+++ lib/Task/Data/Format/Pretty.pm
@@ -0,0 +1,52 @@
+package Task::Data::Format::Pretty;
+1;
+# ABSTRACT: Install Data::Format::Pretty and all known formatters
+
+
+=pod
+
+=head1 NAME
+
+Task::Data::Format::Pretty - Install Data::Format::Pretty and all known formatters
+
+=head1 VERSION
+
+version 0.01
+
+=head1 DESCRIPTION
+
+This task will install Data::Format::Pretty and all Data::Format::Pretty::*
+formatter modules.
+
+=head1 TASK CONTENTS
+
+=head2 Included modules
+
+=head3 L<Data::Format::Pretty>
+
+=head3 L<Data::Format::Pretty::Console>
+
+=head3 L<Data::Format::Pretty::HTML>
+
+=head3 L<Data::Format::Pretty::JSON>
+
+=head3 L<Data::Format::Pretty::YAML>
+
+=head3 L<Data::Format::Pretty::PHP>
+
+=head1 AUTHOR
+
+Steven Haryanto <stevenharyanto@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2011 by Steven Haryanto.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
+
+__END__
+
